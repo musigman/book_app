04508ef49aa82e0c37299954704e5ead8635993c
paul-leonard <paul.ryan.leonard@gmail.com>
2020-09-22 15:04:00 -0700

    results rendering on result page

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get('/', renderSearchPage);
 app.get('/test', renderTestPage);
 app.get('/searches/new', renderSearchPage);
 app.post('/searches', handleNewSearch);
+// app.get('/searches/show', handleSearchResults);
 
 function renderTestPage(request, response) {
   response.render('pages/index');
@@ -34,6 +35,10 @@ function renderTestPage(request, response) {
 function renderSearchPage(request, response) {
   response.render('pages/searches/new.ejs');
 }
+
+// function handleSearchResults(request, response) {
+//   response.render('pages/searches/show.ejs', {arrayOfBookObjects: arrayOfBookObjects});
+// }
 
 function handleNewSearch(request,response) {
   console.log(request.body);
@@ -47,22 +52,23 @@ function handleNewSearch(request,response) {
   superagent
   .get(url)
     .then(data => {
-      console.log(data.body.items);
+      // console.log(data.body.items);
       // response.send(data.body.items);
 
       const arrayOfBooKObjectsFromAPI = data.body.items;
       let arrayOfBookObjects = arrayOfBooKObjectsFromAPI.map( value => new Book(value));
 
-      response.send(arrayOfBookObjects);
-            
+      // response.send(arrayOfBookObjects);
+      // response.redirect('/searches/show'); 
+      response.render('pages/searches/show.ejs', {arrayOfBookObjects: arrayOfBookObjects});
+
     })
     .catch( (error) => {
       response.status(500).send('Sorry, something went wrong  -  error code 6048');
     });
 }
 
-// http://www.google.com
-// https://www.google.com
+
 
 
 
@@ -72,6 +78,36 @@ function Book(bookObject){
   this.authors = bookObject.volumeInfo.authors ? bookObject.volumeInfo.authors : 'No author credited.';
   this.imageURL = bookObject.volumeInfo.imageLinks.smallThumbnail ? bookObject.volumeInfo.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
   this.description = bookObject.volumeInfo.description ? bookObject.volumeInfo.description : "No summary available.";
+
+  // trying to figure out http -> https
+
+  // const str1 = 'http://www.google.com';
+  // const str2 = 'https://www.google.com';
+
+  // let regex = /(http:)/;
+
+  // console.log(str1);
+  // console.log(str2);
+  // console.log(regex);
+
+  // console.log('str1: ',regex.test(str1));
+  // console.log('str2: ',regex.test(str2));
+
+  // if (regex.test(str1) === 1) {
+  //   let imageURL = str1.replace(regex,'https:');
+  //   str1.replace(regex,'https:');
+
+  // } else {
+  //   let imageURL = str1;
+  // }
+
+  // // str.replace(regex,'-');
+  // console.log('end states');
+  // console.log(str1);
+  // console.log(str2);
+  // console.log(regex);
+  // console.log(imageURL);
+
 }
 
 
